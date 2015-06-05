8a4f4cd82a5c4eed6e3d004315f7be2760602ae9
Nyefan <nyefan@aol.com>
2015-06-05 12:43:09 -0500

    Clarify main test for Ch_6.p6_1

--- Programming-in-Lua/src/main.lua
+++ Programming-in-Lua/src/main.lua
@@ -8,17 +8,17 @@ local Ch_6 = require "Ch_6"
 local function main()
   s = function(x) return math.sin(x) end
   c0 = Ch_6.p6_1(s)
-  c = function(a,b) return -1*c0(a,b)+1 end
+  c = function(b) return -1*c0(0,b)+1 end
   
-  print(c(0, -6.28))
-  print(c(0, -4.71))
-  print(c(0, -3.14))
-  print(c(0, -1.57))
-  print(c(0, 0))
-  print(c(0, 1.57))
-  print(c(0, 3.14))
-  print(c(0, 4.71))
-  print(c(0, 6.28))
+  print(c(-6.28))
+  print(c(-4.71))
+  print(c(-3.14))
+  print(c(-1.57))
+  print(c(0))
+  print(c(1.57))
+  print(c(3.14))
+  print(c(4.71))
+  print(c(6.28))
   
 end
 main()
